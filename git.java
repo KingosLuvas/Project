02d1cd1dfae0aab382dad488a3a3be84c3e5547c
lucas <lulu.humbert@free.fr>
2018-03-05 00:07:18 +0100

    on supprime le message

--- git.java
+++ git.java
@@ -2,6 +2,5 @@ public class git {
 	public static void main (String[]args) {
 		
 		System.out.println("Bonjour, j'effectue un test avec Git !");
-		System.out.println("Ceci est un deuxième message !");
 	}
 }
