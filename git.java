2b934b26f9e10f569fd852379ef5a1e8c95b3138
lucas <lulu.humbert@free.fr>
2018-03-05 00:19:53 +0100

    v1.1

--- git.java
+++ git.java
@@ -2,5 +2,9 @@ public class git {
 	public static void main (String[]args) {
 		
 		System.out.println("Bonjour, j'effectue un test avec Git !");
+		int a=2;
+		int b=3;
+		int c = a+b;
+		System.out.println(c);
 	}
 }
